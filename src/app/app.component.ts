ff888e8cf1166d279ba54e46705a5af50e98ba75
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-04-23 10:51:11 -0300

    ideantation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,41 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'control-flow-for';
+  carList = [
+    {
+      make: 'Foyoda',
+      model: 'Famery',
+      miles: 54354,
+      price: 1000,
+      year: 2022,
+      transmission: 'Automatic',
+    },
+    {
+      make: 'Ronda',
+      model: 'Disaccord',
+      miles: 100000,
+      price: 230,
+      year: 1991,
+      transmission: 'Automatic',
+    },
+    {
+      make: 'Specific Motors',
+      model: 'Spoke',
+      miles: 100000,
+      price: 230,
+      year: 1991,
+      transmission: 'Automatic',
+    },
+    {
+      make: 'Fjord',
+      model: 'Pocus',
+      miles: 1,
+      price: 22330,
+      year: 2023,
+      transmission: 'Automatic',
+    },
+  ];
 }
